b0fa39640ffaf2e666b074442afebfcb3cd06f49
agbiotec <agbiotec@bioitx.(none)>
2010-09-09 16:57:54 -0400

    Updated mapper and reducer to include parsing of the Uniref cross-references

---
+++ idmappings_flatten.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+my (%records_hash,@record_fields,$uniref_id,$uniref_map_record);
+open INPUT, "<$ARGV[0]";
+open OUTPUT, ">$ARGV[1]";
+while(<INPUT>)
+{
+	chomp;
+	@record_fields = split;
+	$uniref_id = $record_fields[0];
+	$records_hash{$uniref_id} .= "\t$record_fields[1]-$record_fields[2]";
+}
+while ( ($key, $value) = each %records_hash)
+{
+	print OUTPUT "$key$value\n";
+}
+close INPUT;
+close OUTPUT;
